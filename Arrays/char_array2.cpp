f7a834fe9177549c8a4605a52907d6faf3b27f1c
devsaini889 <ds0752607@gmail.com>
2026-03-26 00:40:29 +0530

    Character Arrays Basic Understanding

---
+++ Arrays/char_array2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main()
+{
+    char arr[100];
+    cout<< "Enter a name: ";
+    cin>> arr;
+    cout << strlen(arr) << endl;
+    
+
+    return 0;
+}
